3b62baf3e5581eb010beec91ecfc3d58563d8555
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2019-12-31 17:50:46 -0800

    Add css modules, contact components

--- src/App.js
+++ src/App.js
@@ -1,65 +1,65 @@
 import React from 'react';
-import AlternateEmail from '@material-ui/icons/AlternateEmail';
-import LinkedIn from '@material-ui/icons/LinkedIn';
-import Twitter from '@material-ui/icons/Twitter';
 import logo from './logo.svg';
-import './App.css';
+import ContactInfo from './Components/ContactInfo/ContactInfo';
+import styles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App__header">
-        <img src={logo} className="App__logo" alt="logo" />
+    <>
+      <header className={styles.header}>
+        <img src={logo} className={styles.logo} alt="logo" />
         <h1>Ariel Kaplan</h1>
-        <ul>
-          <li>
-            <a
-              className="App__link"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <AlternateEmail titleAccess="Email Ariel" />
-            </a> 
-          </li>
-          <li>
-            <a
-              className="App__link"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Twitter titleAccess="Twitter Account (opens in a new tab)" />
-            </a> 
-          </li>
-          <li>
-            <a
-              className="App__link"
-              href="#"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedIn titleAccess="LinkedIn Profile (opens in a new tab)" />
-            </a> 
-          </li>
-        </ul>
+        <ContactInfo />
       </header>
-      <main>
+      <main className={styles.main}>
         <section>
           <h2>Hello</h2>
-          <p>Stuff</p>
+          <p>The only home we’ve ever known preserve and cherish that pale blue dot. Cosmic fugue, circumnavigated descended from astronomers decipherment, permanence of the stars science Euclid muse about!</p>
+          <p>A still more glorious dawn awaits Euclid, tendrils of gossamer clouds extraplanetary muse about vastness is bearable only through love Cambrian explosion! Extraordinary claims require extraordinary evidence of brilliant syntheses? Take root and flourish, stirred by starlight billions upon billions Drake Equation.</p>
         </section>
         <section>
           <h2>Projects</h2>
           <p>Stuff</p>
+          <ul>
+            <li>
+              <article>
+                <h3>Project 1</h3>
+                <p>Stuff</p>
+              </article>
+            </li>
+            <li>
+              <article>
+                <h3>Project 2</h3>
+                <p>Stuff</p>
+              </article>
+            </li>
+            <li>
+              <article>
+                <h3>Project 3</h3>
+                <p>Stuff</p>
+              </article>
+            </li>
+            <li>
+              <article>
+                <h3>Project 4</h3>
+                <p>Stuff</p>
+              </article>
+            </li>
+            <li>
+              <article>
+                <h3>Project 5</h3>
+                <p>Stuff</p>
+              </article>
+            </li>
+          </ul>
         </section>
         <section>
           <h2>Learning</h2>
           <p>Stuff</p>
         </section>
       </main>
-      <footer>&copy; 2019 Ariel Kaplan</footer>
-    </div>
+      <footer className={styles.footer}>&copy; 2019 Ariel Kaplan</footer>
+    </>
   );
 }
 
