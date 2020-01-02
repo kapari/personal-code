aca8563a2a99f086fbefa9e7638a81804a601eaa
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2020-01-01 21:21:16 -0800

    Add proptypes

--- src/Components/ContactInfo/ContactItem/ContactItem.js
+++ src/Components/ContactInfo/ContactItem/ContactItem.js
@@ -1,20 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './ContactItem.module.css';
 
-const ContactItem = (props) => {
+const ContactItem = ({url, title, iconElt}) => {
     return (
         <li className={styles.item}>
             <a
                 className={styles.link}
-                href={props.url || "#"}
+                href={url}
                 target="_blank"
                 rel="noopener noreferrer"
-                title={props.title}
+                title={title}
             >
-                {props.iconElt}
+                {iconElt}
             </a>
         </li>
     );
+}
+
+ContactItem.propTypes = {
+    url: PropTypes.string,
+    title: PropTypes.string,
+    iconElt: PropTypes.node.isRequired
+}
+
+ContactItem.defaultProps = {
+    url: "/",
+    title: "Home"
 }
 
 export default ContactItem;
