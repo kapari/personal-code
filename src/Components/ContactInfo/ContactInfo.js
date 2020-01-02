0fa928a0e55f69821e0ad203db2c57162b4fe77f
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2020-01-01 21:25:21 -0800

    Add github contact link

--- src/Components/ContactInfo/ContactInfo.js
+++ src/Components/ContactInfo/ContactInfo.js
@@ -4,6 +4,7 @@ import SvgIcon from '@material-ui/core/SvgIcon';
 import AlternateEmail from '@material-ui/icons/AlternateEmail';
 import LinkedIn from '@material-ui/icons/LinkedIn';
 import Twitter from '@material-ui/icons/Twitter';
+import GitHub from '@material-ui/icons/GitHub';
 import styles from './ContactInfo.module.css';
 
 const contactLinks = [
@@ -21,11 +22,15 @@ const contactLinks = [
         icon: LinkedIn,
         title: "LinkedIn Profile (opens in a new tab)",
         url: "#"
+    },
+    {
+        icon: GitHub,
+        title: "GitHub Profile (opens in a new tab",
+        url: "#"
     }
 ];
 
 const ContactInfo = () => {
-
     return (
         <ul className={styles.list}>
             {contactLinks.map((item, index) => (
@@ -36,7 +41,7 @@ const ContactInfo = () => {
                     iconElt={<SvgIcon component={item.icon} titleAccess={item.title} />}
                 />
             ))}
-        </ul>
+        </ul>   
     )
 }
 
